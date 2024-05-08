acb7b4351609b4b8e57b0eee978315995bfa09a9
Nicknul <168498620+Nicknul@users.noreply.github.com>
2024-05-08 18:10:21 +0900

    HTML and js file to create

---
+++ Node-2/server.js
@@ -0,0 +1,12 @@
+const { title } = require("process");
+
+let root = document.getElementById('root');
+// console.log(root);
+
+let newH1 = document.createElement('h1');
+newH1.innerHTML = "I'm Hoyeon";
+root.appendChild(newH1);
+console.log(newH1);
+
+
+alert('hello world');
