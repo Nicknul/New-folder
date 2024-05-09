3e9641a861b3a806edc3024d6f3c8c1921d9a3a4
Nicknul <168498620+Nicknul@users.noreply.github.com>
2024-05-09 18:09:59 +0900

    lunchMenu random

---
+++ Node-3/lunch-selector-ex.js
@@ -0,0 +1,26 @@
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+}
+
+// getRandomInt(0, array.length);
+function lunchSelector(array) {
+  const firstGetRandomIndex = getRandomInt(0, array.length);
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[firstGetRandomIndex])) {
+      const randomIndex = getRandomInt(0, array[i].length);
+      for (let j = 0; j < array[i].length; j++) {
+        return array[firstGetRandomIndex][randomIndex];
+      }
+      return array[randomIndex];
+    }
+  }
+  return array[firstGetRandomIndex];
+}
+console.log(lunchSelector(lunchMenus));
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(1, 2));
